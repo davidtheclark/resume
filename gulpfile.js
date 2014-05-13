99e2da238211cb091bfbee7283cf2ebe9761cc60
David Clark <david.dave.clark@gmail.com>
2014-05-12 21:07:44 -0700

    Polishing tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,7 @@ var uncss = require('gulp-uncss');
 gulp.task('style', function() {
   gulp.src('./scss/main.scss')
     .pipe(sass())
-    .pipe(prefix(['last 3 versions', '> 1%', 'ie 8']))
-    // .pipe(uncss({
-    //   html: 'index.html'
-    // }))
+    .pipe(prefix(['last 3 versions', '> 1%', 'ie 9']))
     .pipe(minifycss({keepSpecialComments: 0}))
     .pipe(gulp.dest('./css'));
 });
